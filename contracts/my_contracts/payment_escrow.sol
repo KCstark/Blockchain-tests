6a230fe8b4def1006dc91c004c6cbe1b32c6f6ee
KCstark <kanishakc1@gmail.com>
2025-12-09 19:27:39 +0530

    starting practice for escrow and payment splitter contract

---
+++ contracts/my_contracts/payment_escrow.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+contract Escrow{
+    address owner;
+
+    constructor () {
+        owner=msg.sender;
+    }
+
+    struct User {
+            address userAddress;
+            string name;
+            uint256 balance;
+            //list of trade
+            string[] trade;
+    }
+
+    function deposit(uint amount) public payable {
+        require(msg.value == amount, "Amount must match the value sent");
+    }
+}
